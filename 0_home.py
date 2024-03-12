d99ffbec78f3f4186bf930f2d7c9cdeaf45004a8
annieycchiu <annieycchiu@gmail.com>
2024-03-12 01:19:14 -0700

    add notations

--- 0_home.py
+++ 0_home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.other_utils import add_logo, display_formulas
+from utils.other_utils import add_logo, display_homepage_formulas
 from utils.formulas import *
 
 
@@ -19,8 +19,9 @@ def main():
     st.write('')
 
     # Binomial distribution
-    display_formulas(
+    display_homepage_formulas(
         '01. Binomial Distribution',
+        binomial_notation,
         binomial_pmf, binomial_cdf,
         binomial_exp, binomial_var,
         type='Discrete'
@@ -28,8 +29,9 @@ def main():
     st.divider()
 
     # Poisson distribution
-    display_formulas(
+    display_homepage_formulas(
         '02. Poisson Distribution',
+        poisson_notation,
         poisson_pmf, poisson_cdf,
         poisson_exp, poisson_var,
         type='Discrete'
@@ -37,8 +39,9 @@ def main():
     st.divider()
 
     # Uniform distribution
-    display_formulas(
+    display_homepage_formulas(
         '03. Uniform Distribution',
+        uniform_notation,
         uniform_pdf, uniform_cdf,
         uniform_exp, uniform_var,
         type='Continuous'
@@ -46,28 +49,31 @@ def main():
     st.divider()
 
     # Normal (Gaussian) distribution
-    display_formulas(
+    display_homepage_formulas(
         '04. Normal (Gaussian) Distribution',
+        normal_notation,
         normal_pdf, normal_cdf,
         normal_exp, normal_var,
         type='Continuous'
         )
     st.divider()
 
-    # Gamma distribution
-    display_formulas(
-        '05. Gamma Distribution',
-        gamma_pdf, gamma_cdf,
-        gamma_exp, gamma_var,
+    # Exponential distribution
+    display_homepage_formulas(
+        '05. Exponential Distribution',
+        exponential_notation,
+        exponential_pdf, exponential_cdf,
+        exponential_exp, exponential_var,
         type='Continuous'
         )
     st.divider()
 
-    # Uniform distribution
-    display_formulas(
-        '06. Uniform Distribution',
-        uniform_pdf, uniform_cdf,
-        uniform_exp, uniform_var,
+    # Gamma distribution
+    display_homepage_formulas(
+        '06. Gamma Distribution',
+        gamma_notation,
+        gamma_pdf, gamma_cdf,
+        gamma_exp, gamma_var,
         type='Continuous'
         )
 
