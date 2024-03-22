75841b63a4016aeb46d319a0abb0b4f71cdd1e7d
annieycchiu <annieycchiu@gmail.com>
2024-03-21 22:41:06 -0700

    update layout

---
+++ pages/13_13_bootstrapping.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+from utils.other_utils import add_logo, setup_sticky_header, add_title
+
+def main():
+    # Set up the layout of Streamlit app
+    st.set_page_config(
+        page_title='Bootstrapping',
+        page_icon=':bar_chart:',
+        layout='wide',
+        initial_sidebar_state='collapsed'
+    )
+
+    # Add USF logo at sidebar
+    add_logo()
+
+    # Define sticky header
+    header = st.container()
+    with header:
+        title = 'Bootstrapping'
+        add_title(title)
+
+        st.write("<div class='fixed-header'/>", unsafe_allow_html=True)
+
+    # Set up sticky header
+    setup_sticky_header(header)
+
+
+if __name__ == '__main__':
+    main()
