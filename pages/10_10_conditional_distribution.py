e57a14fde73ed94c35762cb3e825e247462b1f5b
annieycchiu <annieycchiu@gmail.com>
2024-05-07 06:09:44 -0700

    update layout

--- pages/10_10_conditional_distribution.py
+++ pages/10_10_conditional_distribution.py
@@ -3,6 +3,42 @@ import pandas as pd
 
 from utils.other_utils import add_logo, setup_sticky_header, add_title
 
+# Create sample dataset
+df = pd.DataFrame({
+    'Stats & Prob': [22, 8, 30],
+    'Python': [8, 4, 12],
+    'EDA': [13, 8, 21],
+    'Machine Learning': [15, 10, 25],
+    'Database': [7, 5, 12],
+    'Total': [65, 35, 100],
+})
+
+row_names = ['Male', 'Female', 'Total']
+df.index = row_names
+
+# A function to highlight a row based on index
+def highlight_row(row, selected_row):
+    # Check if the row name is the selected one
+    if row.name == selected_row:
+        # Highlight all values in USF Green, except the "Total" column in USF Yellow
+        USF_Green = 'rgba(0, 84, 60, 0.9)'
+        USF_Yellow = 'rgba(253, 187, 48, 0.5)'
+        return [f'background-color: {USF_Green}; color:white' if col != 'Total' else f'background-color: {USF_Yellow}' for col in row.index]
+    else:
+        return ['' for col in row.index]
+
+# A function to highlight a column based on column name
+def highlight_column(column, selected_col):
+    # Check if the column name is the selected one
+    if column.name == selected_col:
+        # Highlight all values in USF Green, except the "Total" column in USF Yellow
+        USF_Green = 'rgba(0, 84, 60, 0.9)'
+        USF_Yellow = 'rgba(253, 187, 48, 0.5)'
+        return [f'background-color: {USF_Green}; color:white' if idx != 'Total' else f'background-color: {USF_Yellow}' for idx in column.index]
+    else:
+        return ['' for idx in column.index]
+
+
 def main():
     # Set up the layout of Streamlit app
     st.set_page_config(
@@ -30,7 +66,6 @@ def main():
         )
         
         col1, col2 = st.columns([0.1, 0.9])
-
         with col1:
             st.write("<span style='color:#00543C; font-size:18px; font-weight:bold;'>Y</span>", unsafe_allow_html=True)
             st.write("<span style='color:#FDBB30; font-size:18px; font-weight:bold;'>X</span>", unsafe_allow_html=True)
@@ -50,20 +85,14 @@ def main():
     # Set up sticky header
     setup_sticky_header(header)
 
-    # Create sample dataset
-    df = pd.DataFrame({
-        'Stats & Prob': [22, 8, 30],
-        'Python': [8, 4, 12],
-        'EDA': [13, 8, 21],
-        'Machine Learning': [15, 10, 25],
-        'Database': [7, 5, 12],
-        'Total': [65, 35, 100],
-    })
+    # Stylize dataframe based on user selection        
+    if Y == 'Course':
+        styled_df = df.style.apply(highlight_row, selected_row=X_val, axis=1)
 
-    row_names = ['Male', 'Female', 'Total']
-    df.index = row_names
+    elif Y == 'Gender':
+        styled_df = df.style.apply(highlight_column, selected_col=X_val, axis=0)
 
-    st.dataframe(df, use_container_width=True)
+    st.dataframe(styled_df, use_container_width=True)
     
     st.write(
         f"<span style='font-size:20px;'>The conditional distribution of "
@@ -73,6 +102,7 @@ def main():
         unsafe_allow_html=True
     )
 
+    # Display calculation process of each probability
     if Y == 'Course':
         Ys = df.columns[:-1]
         counts = df.loc[X_val][:-1]
